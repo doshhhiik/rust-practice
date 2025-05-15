688af847dd55959fa9ccd3dec4193011285d9c4b
Dasha <129966294+doshhhiik@users.noreply.github.com>
2025-05-15 14:43:10 +0300

    task01.rs

--- src/hackerrank/task01.rs
+++ src/hackerrank/task01.rs
@@ -1,4 +1,8 @@
-#[test]
-fn test1() {
-    assert_eq!(1 + 1, 2);
+fn simple_array_sum(arr: &[i32]) -> i32 {
+    arr.iter().sum()
+}
+
+fn main() {
+    let numbers = vec![1, 2, 3, 4, 10, 11];
+    println!("{}", simple_array_sum(&numbers)); // Виведе 31
 }
